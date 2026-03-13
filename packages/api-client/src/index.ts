6f07f64222b14ed9851553528ba4eb8deb08cebc
Jigoooo <jiwook97@gmail.com>
2026-03-13 11:58:03 +0900

    export 수정

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -1,7 +1,4 @@
 export { initApi } from './init-api';
 export type { ApiConfig } from './config';
 
-export { customedAxios } from './customed-axios';
 export { api } from './api';
-
-export { apiRequest } from './api-request';
